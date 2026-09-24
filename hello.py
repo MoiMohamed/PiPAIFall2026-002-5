828e1e800fcc39c02b2a51a9267d38b4221570f0
MoiMohamed <mohamed.mahmoud@nyu.edu>
2026-09-23 21:51:49 -0400

    Add Mohamed to the names list

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-names = ["Ali Shehadi", "Dev Ganguli", "name3", 'name4', 'name5']
+names = ["Ali Shehadi", "Dev Ganguli", "Mohamed Mahmoud", 'name4', 'name5']
 
 print(f"Hello, World! Our group members are: {names}")
